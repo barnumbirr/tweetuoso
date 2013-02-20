71952cbc2eef7824c8bb7d0e6bb71c915906fa50
c0ding <martin.simon@email.com>
2013-02-20 07:37:51 +0100

    removed keys :D

--- tweetuoso/api_rate_limit.py
+++ tweetuoso/api_rate_limit.py
@@ -8,10 +8,10 @@ __licence__ = "WTFPL"
 
 import tweepy as tw
 
-consumer_key = 'm4S43wXKmfWR31lgwzRJg'
-consumer_secret = 'nZwQYsGXa7Vg7KBltf6aw8ao3GV8ltCTPjje4Rwrr6k'
-access_token = '636598354-pfEmyHsAR9FuUVVv3uEdkdepB5gsuo7CcizZiV7Y' 
-access_secret = 'Os6RDzY7ufE3hp8pL7gGbUFfHOniGMzs9s3Ae3BYE3o'
+consumer_key = ''
+consumer_secret = ''
+access_token = '' 
+access_secret = ''
 
 def rate_limit():
 	auth = tw.OAuthHandler(consumer_key, consumer_secret)
