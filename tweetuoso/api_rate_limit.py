6535000674eb4d694819db2d4a1eea828fc70865
c0ding <martin.simon@email.com>
2013-02-24 16:10:15 +0100

    Updated api_rate_limit for auth.py

--- tweetuoso/api_rate_limit.py
+++ tweetuoso/api_rate_limit.py
@@ -2,26 +2,25 @@
 # -*- coding: utf-8 -*-
 
 __appname__ = 'api_rate_limit'
-__version__ = "0.2"
+__version__ = "0.3"
 __author__ = "@c0ding <https://twitter.com/c0ding>"
 __licence__ = "WTFPL"
 
+import os
 import tweepy as tw
-
-consumer_key = ''
-consumer_secret = ''
-access_token = '' 
-access_secret = ''
+from auth import keys
+from colorama import Fore, Style
 
 def rate_limit():
 	try:
-		auth = tw.OAuthHandler(consumer_key, consumer_secret)
-		auth.set_access_token(access_token, access_secret)
+		auth = tw.OAuthHandler(keys['consumer_key'], keys['consumer_secret'])
+		auth.set_access_token(keys['access_token'], keys['access_secret'])
 		api = tw.API(auth)
 		print api.rate_limit_status()
 		
-	except tw.TweepError:
-		print "Error occured, failed to get token"
+	except tw.TweepError as error:
+		os.system('clear')
+		print(Fore.RED + ">> " + Fore.RESET + "Error occured: %s" % error)
 		return
 					
 if __name__ == '__main__':
